1bdd8aeb7e3aab77ed4026d94afdef9755cae615
chocoMJ <nkmj0623r222@gmail.com>
2025-09-09 20:44:19 +0900

    feat : 카메라 추가
    플레이어에게 기본적으로 붙어야하는 카메라 컴포넌트 추가

--- Source/Return_Paragon/Public/Characters/ParagonHeroCharacter.h
+++ Source/Return_Paragon/Public/Characters/ParagonHeroCharacter.h
@@ -6,6 +6,9 @@
 #include "Characters/ParagonBaseCharacter.h"
 #include "ParagonHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
@@ -14,6 +17,18 @@ class RETURN_PARAGON_API AParagonHeroCharacter : public AParagonBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AParagonHeroCharacter();
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+#pragma endregion
 };
