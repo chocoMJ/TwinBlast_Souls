32ae72a3121fbab91f3f16d0379c2cfc40ccbbf3
chocoMJ <nkmj0623r222@gmail.com>
2025-09-10 18:18:38 +0900

    feat : GameplayTag 추가
    GameplayTag를 선언 및 정의하긴 위한 소스파일 구현 및 Look과 Move에 해당하는 태그 선언.
    이를 위한 Build.cs 파일 수정

---
+++ Source/Return_Paragon/Public/ParagonGameplayTags.h
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+#include "NativeGameplayTags.h"
+
+namespace ParagonGameplayTags
+{
+	/* Input Tags */
+	RETURN_PARAGON_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	RETURN_PARAGON_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
