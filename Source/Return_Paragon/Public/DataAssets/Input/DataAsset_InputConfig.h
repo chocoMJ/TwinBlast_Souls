7261951704eae6d3ffba2571c250a171084eb8ca
chocoMJ <nkmj0623r222@gmail.com>
2025-09-11 22:39:30 +0900

    feat : InputAction과 GameplayTag 매핑
    DataAsset_InputConfig를 통해 InputTag에 해당하는 게임플레이태그와 InputAction 매핑

---
+++ Source/Return_Paragon/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FParagonInputActionConfig
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class RETURN_PARAGON_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (TitleProperty = "InputTag"))
+	TArray<FParagonInputActionConfig> NativeInputActions;
+
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+};
