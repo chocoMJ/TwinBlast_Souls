cb06897b3dae444406763c75994c7522d14d5af3
chocoMJ <nkmj0623r222@gmail.com>
2025-09-12 21:37:10 +0900

    feat : InputComponent 생성
    EnhancedInputComponent를 상속받아 컴포넌트 생성. 인풋액션과 실제함수 바인딩 할 수 있는 함수 생성.
    
    + DataAsset_InputConfig의 FindNativeInputActionByTag함수를 const함수로 수정

---
+++ Source/Return_Paragon/Public/Components/Input/ParagonInputComponent.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "ParagonInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RETURN_PARAGON_API UParagonInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UParagonInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input config data asset is null"));
+
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
