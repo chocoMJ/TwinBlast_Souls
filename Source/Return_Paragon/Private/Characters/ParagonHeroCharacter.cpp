ef1540f26d7f27ff9fd573f568db45edca599ad2
chocoMJ <nkmj0623r222@gmail.com>
2025-09-12 22:54:35 +0900

    feat : InputAction에 바인딩될 콜백함수 생성 후 바인딩
    IA_Look과 IA_Move를 InputAction과 바인딩

--- Source/Return_Paragon/Private/Characters/ParagonHeroCharacter.cpp
+++ Source/Return_Paragon/Private/Characters/ParagonHeroCharacter.cpp
@@ -6,6 +6,10 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "Components/Input/ParagonInputComponent.h"
+#include "ParagonGameplayTags.h"
 
 #include "ParagonDebugHelper.h"
 
@@ -33,7 +37,61 @@ AParagonHeroCharacter::AParagonHeroCharacter()
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 }
 
+void AParagonHeroCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	checkf(InputConfigDataAsset, TEXT("Forgot to assign a valid data asset as input config"));
+
+	ULocalPlayer* LocalPlayer = GetController<APlayerController>()->GetLocalPlayer();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(LocalPlayer);
+
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(InputConfigDataAsset->DefaultMappingContext, 0);
+
+	UParagonInputComponent* ParagonInputComponent = CastChecked<UParagonInputComponent>(PlayerInputComponent);
+
+	ParagonInputComponent->BindNativeInputAction(InputConfigDataAsset, ParagonGameplayTags::InputTag_Move, ETriggerEvent::Triggered, this, &ThisClass::Input_Move);
+	ParagonInputComponent->BindNativeInputAction(InputConfigDataAsset, ParagonGameplayTags::InputTag_Look, ETriggerEvent::Triggered, this, &ThisClass::Input_Look);
+}
+
 void AParagonHeroCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AParagonHeroCharacter::Input_Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2D MovementVector = InputActionValue.Get<FVector2D>();
+
+	const FRotator MovementRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+
+	if (MovementVector.Y != 0.f)
+	{
+		const FVector ForwardDirection = MovementRotation.RotateVector(FVector::ForwardVector);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+	}
+
+	if (MovementVector.X != 0.f)
+	{
+		const FVector RightDirection = MovementRotation.RotateVector(FVector::RightVector);
+
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AParagonHeroCharacter::Input_Look(const FInputActionValue& InputActionValue)
+{
+	const FVector2D LookAxisVector = InputActionValue.Get<FVector2D>();
+
+	if (LookAxisVector.X != 0.f)
+	{
+		AddControllerYawInput(LookAxisVector.X);
+	}
+
+	if (LookAxisVector.Y != 0.f)
+	{
+		AddControllerPitchInput(-LookAxisVector.Y);
+	}
 }
